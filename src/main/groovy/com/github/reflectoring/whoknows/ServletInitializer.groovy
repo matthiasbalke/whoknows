612d5d94c8359d00487c8158d72fdaaab750cc2e
Matthias Balke <matthias.balke@googlemail.com>
2017-03-24 19:43:28 +0100

    initial project setup

---
+++ src/main/groovy/com/github/reflectoring/whoknows/ServletInitializer.groovy
@@ -0,0 +1,13 @@
+package com.github.reflectoring.whoknows
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+
+class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.sources(WhoknowsApplication)
+	}
+
+}
---
+++ src/main/groovy/com/github/reflectoring/whoknows/WhoknowsApplication.groovy
@@ -0,0 +1,12 @@
+package com.github.reflectoring.whoknows
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WhoknowsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run WhoknowsApplication, args
+	}
+}
