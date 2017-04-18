96d66d0e80e2d9f4e0a12855707b7b3d7d7c38af
Matthias Balke <matthias.balke@gmail.com>
2017-04-18 23:14:45 +0200

    fixed github-release path

--- github-release.sh
+++ github-release.sh
@@ -2,13 +2,15 @@
 
 # install github-release tool
 curl -L -o ~/github-release.tar.bz2 https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
-tar xjf ~/github-release.tar.bz2
+tar -C ~ -jxf ~/github-release.tar.bz2
 export PATH=~/bin/linux/amd64:"$PATH"
 
 ARTIFACT="$(echo $CIRCLE_ARTIFACTS/build-artifacts/backend-*.war)"
 TAG_VERSION=$CIRCLE_TAG
 
-github-release release \
+test -f "$ARTIFACT"
+
+~/github-release release \
   --user matthiasbalke \
   --repo whoknows \
   --tag "$TAG_VERSION" \
@@ -16,7 +18,7 @@ github-release release \
   --description "Released on $(date +%Y-%m-%d)" \
   --draft
 
-github-release upload \
+~/github-release upload \
   --user matthiasbalke \
   --repo whoknows \
   --tag "$TAG_VERSION" \
