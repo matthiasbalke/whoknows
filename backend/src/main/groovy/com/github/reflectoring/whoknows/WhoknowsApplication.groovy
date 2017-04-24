e3c4a1235a25eb45c6352132795b8cfd0bb648f2
Matthias Balke <matthias.balke@gmail.com>
2017-04-24 17:49:13 +0200

    configure CORS

--- backend/src/main/groovy/com/github/reflectoring/whoknows/WhoknowsApplication.groovy
+++ backend/src/main/groovy/com/github/reflectoring/whoknows/WhoknowsApplication.groovy
@@ -3,9 +3,13 @@ package com.github.reflectoring.whoknows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 class WhoknowsApplication extends WebSecurityConfigurerAdapter {
@@ -26,13 +30,29 @@ class WhoknowsApplication extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .permitAll()
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser("user").password("password").roles("USER")
+    }
+
+    /**
+     * Activates Cross-Origin Resource Sharing (CORS)
+     * for local development of JavaScript client.
+     *
+     * https://de.wikipedia.org/wiki/Cross-Origin_Resource_Sharing
+     */
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**/*").allowedOrigins("http://localhost:4200")
+            }
+        }
     }
 }
